8c7dffc22acde663954682a6e3d3aead44567ba8
Imhunwoo <hunwoo0505@gmail.com>
2019-11-27 23:29:06 +0900

    first page

---
+++ frontend/src/component/First.js
@@ -0,0 +1,16 @@
+import "./First.css";
+import React from "react";
+
+class First extends React.Component {
+  render() {
+    return (
+      <div className="background">
+        <span className="title">hello</span>
+        <br></br>
+        <button>start!</button>
+      </div>
+    );
+  }
+}
+
+export default First;
