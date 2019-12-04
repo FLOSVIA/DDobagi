e247dd5988d462420b8c3a2f76c5871e53da2ad3
fldfls <49551135+fldfls@users.noreply.github.com>
2019-12-04 20:21:03 +0900

    Create First.js

---
+++ frontend/src/component/First/First.js
@@ -0,0 +1,25 @@
+import "./First.css";
+import React from "react";
+
+import { Link, Router, Route, BrowserRouter, Switch } from "react-router-dom";
+
+class First extends React.Component {
+  render() {
+    return (
+      <div className="background">
+        <BrowserRouter>
+          <React.Fragment>
+            <span className="hi">hello</span>
+            <Link to="/Main">
+              <button>
+                <span>Login</span>
+              </button>
+            </Link>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default First;
