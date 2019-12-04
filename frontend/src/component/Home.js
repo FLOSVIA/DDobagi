da1cd5d5c80f063ccb5fd045a47959edf9389ceb
Imhunwoo <hunwoo0505@gmail.com>
2019-12-04 16:56:53 +0900

    Update Home.js

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="background">
-        <span className="title">home page</span>
+        <span className="title">home page2</span>
       </div>
     );
   }
