5a61c5cd15ffe845f0174bc74d41de136e78f03b
Imhunwoo <hunwoo0505@gmail.com>
2019-12-10 16:43:47 +0900

    fix/first

--- frontend/src/component/First/First.js
+++ frontend/src/component/First/First.js
@@ -7,23 +7,19 @@ class First extends Component {
   render() {
     return (
       <div className="background">
-        <BrowserRouter>
-          <React.Fragment>
-            <span className="a">
-              <b className="hello">Hello</b>, Let's Make{" "}
-              <b className="last">Plan</b>!
-            </span>
-            <Link to="/Main">
-              <button>
-                <span>Start</span>
-              </button>
-            </Link>
-            <br></br>
-            <Link to="/Quick">
-              <span className="qst">Quick Start</span>
-            </Link>
-          </React.Fragment>
-        </BrowserRouter>
+        <span className="a">
+          <b className="hello">Hello</b>, Let's Make{" "}
+          <b className="last">Plan</b>!
+        </span>
+        <Link to="/Main">
+          <button>
+            <span>Start</span>
+          </button>
+        </Link>
+        <br></br>
+        <Link to="/Quick">
+          <span className="qst">Quick Start</span>
+        </Link>
       </div>
     );
   }
