186f567cf6f1a89ab41c82eeacbecd8c461e8040
Imhunwoo <hunwoo0505@gmail.com>
2019-12-23 18:46:58 +0900

    Update Main.js

--- frontend/src/component/Main/Main.js
+++ frontend/src/component/Main/Main.js
@@ -5,8 +5,12 @@ class Main extends React.Component {
   render() {
     return (
       <div className="background">
-        <span className="title">home page</span>
-        <button className="check">새로운 버튼</button>
+        <center>
+          <span className="jemok">Hello, Let's Make Plan!</span>
+
+          <div className="month12"></div>
+          <div className="month1"></div>
+        </center>
       </div>
     );
   }
